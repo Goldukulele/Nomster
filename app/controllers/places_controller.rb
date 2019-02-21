7119345658aab61e88241c9cfa1a4ea9d9f61229
GoldUkulele <kcramos1211@gmail.com>
2019-02-21 05:59:32 +0000

    Creating the place form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,6 +2,12 @@ class PlacesController < ApplicationController
 
   def index
     @places = Place.all
+    @products = Place.order("name").page(params[:page]).per_page(5)
+
+  end
+
+  def new
+    @place = Place.new
   end
 
 end
